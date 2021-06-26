5b06f342f28e9e17c1da2b66233d46f64e674523
xiaoyi <2217021563@qq.com>
2021-06-26 15:31:24 +0800

    HR管理系统v1.0.3 主要更新内容： 1.更新数据库

--- hrsystem.sql
+++ hrsystem.sql
@@ -3,15 +3,15 @@
 
  Source Server         : crc
  Source Server Type    : MySQL
- Source Server Version : 80021
+ Source Server Version : 80022
  Source Host           : localhost:3306
  Source Schema         : hrsystem
 
  Target Server Type    : MySQL
- Target Server Version : 80021
+ Target Server Version : 80022
  File Encoding         : 65001
 
- Date: 19/06/2021 18:13:58
+ Date: 25/06/2021 13:30:46
 */
 
 SET NAMES utf8mb4;
@@ -23,37 +23,70 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `tbl_department`;
 CREATE TABLE `tbl_department`  (
   `dept_id` int(0) NOT NULL AUTO_INCREMENT,
-  `dept_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `dept_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
   PRIMARY KEY (`dept_id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 44 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tbl_department
 -- ----------------------------
-INSERT INTO `tbl_department` VALUES (1, '开发部');
-INSERT INTO `tbl_department` VALUES (2, '营销部');
-INSERT INTO `tbl_department` VALUES (3, '事业部');
-INSERT INTO `tbl_department` VALUES (4, '营销部');
-INSERT INTO `tbl_department` VALUES (5, '宣传部');
+INSERT INTO `tbl_department` VALUES (40, '运营部');
+INSERT INTO `tbl_department` VALUES (41, '财务部');
+INSERT INTO `tbl_department` VALUES (42, '营销部');
+INSERT INTO `tbl_department` VALUES (43, '开发部');
 
 -- ----------------------------
 -- Table structure for tbl_employee
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_employee`;
 CREATE TABLE `tbl_employee`  (
-  `emp_id` int unsigned NOT NULL,
-  `emp_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `emp_email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `emp_id` int(0) NOT NULL AUTO_INCREMENT,
+  `emp_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `emp_email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
   `emp_gender` bit(1) NULL DEFAULT NULL,
-  `d_id` int(0) NULL DEFAULT NULL,
+  `d_id` int(0) NOT NULL,
   PRIMARY KEY (`emp_id`) USING BTREE,
-  INDEX `d_id`(`d_id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+  INDEX `d_id`(`d_id`) USING BTREE,
+  CONSTRAINT `tbl_employee_ibfk_1` FOREIGN KEY (`d_id`) REFERENCES `tbl_department` (`dept_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 75 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tbl_employee
 -- ----------------------------
-INSERT INTO `tbl_employee` VALUES (1, '李桂华', '21213@qq.com', b'1', 1);
+INSERT INTO `tbl_employee` VALUES (53, '刘德华', '20221612@qq.com', b'0', 41);
+INSERT INTO `tbl_employee` VALUES (54, '徐毅', '121312@qq.com', b'1', 42);
+INSERT INTO `tbl_employee` VALUES (75, '流年', '2217021563@qq.com', b'0', 40);
+INSERT INTO `tbl_employee` VALUES (76, '陈伟霆', '2487534069@qq.com', b'0', 41);
+INSERT INTO `tbl_employee` VALUES (77, '刘恺威', '1277771312@qq.com', b'0', 42);
+INSERT INTO `tbl_employee` VALUES (78, '莫文蔚', '1277771312@qq.com', b'1', 41);
+INSERT INTO `tbl_employee` VALUES (79, '钟汉良', '1277771312@qq.com', b'0', 41);
+INSERT INTO `tbl_employee` VALUES (80, '叶轩', '1277771312@qq.com', b'1', 41);
+INSERT INTO `tbl_employee` VALUES (81, '洪金宝', '2217021563@qq.com', b'0', 43);
+INSERT INTO `tbl_employee` VALUES (82, '成龙', '1277771312@qq.com', b'0', 41);
+INSERT INTO `tbl_employee` VALUES (83, '林子聪', '2217021563@qq.com', b'0', 41);
+INSERT INTO `tbl_employee` VALUES (84, '苗侨伟', '1277771312@qq.com', b'0', 41);
+INSERT INTO `tbl_employee` VALUES (85, '曾志伟', '2217021563@qq.com', b'0', 41);
+INSERT INTO `tbl_employee` VALUES (86, '任达华', '1277771312@qq.com', b'0', 42);
+INSERT INTO `tbl_employee` VALUES (87, '傻强', '1277771312@qq.com', b'1', 41);
+INSERT INTO `tbl_employee` VALUES (88, '余诗曼', '2217021563@qq.com', b'1', 41);
+INSERT INTO `tbl_employee` VALUES (89, '余文乐', '2217021563@qq.com', b'0', 40);
+INSERT INTO `tbl_employee` VALUES (90, '林凡', '1277771312@qq.com', b'0', 40);
+INSERT INTO `tbl_employee` VALUES (91, '米雪', '2217021563@qq.com', b'1', 43);
+INSERT INTO `tbl_employee` VALUES (92, '邓紫棋', '2217021563@qq.com', b'1', 42);
+INSERT INTO `tbl_employee` VALUES (93, '莫少聪', '1277771312@qq.com', b'0', 41);
+INSERT INTO `tbl_employee` VALUES (95, '古天乐', '2217021563@qq.com', b'0', 41);
+INSERT INTO `tbl_employee` VALUES (96, '樊少皇', '2487534069@qq.com', b'0', 42);
+INSERT INTO `tbl_employee` VALUES (97, '张晋', '1217021563@qq.com', b'1', 43);
+INSERT INTO `tbl_employee` VALUES (98, '吴宣仪', '2217021563@qq.com', b'1', 41);
+INSERT INTO `tbl_employee` VALUES (99, '甄子丹', '1277771312@qq.com', b'0', 41);
+INSERT INTO `tbl_employee` VALUES (100, '吴千语', '2217021563@qq.com', b'0', 41);
+INSERT INTO `tbl_employee` VALUES (101, '陈浩明', '1277771312@qq.com', b'0', 43);
+INSERT INTO `tbl_employee` VALUES (102, '徐晶晶', '2217021563@qq.com', b'1', 42);
+INSERT INTO `tbl_employee` VALUES (103, '徐锦江', '2487534069@qq.com', b'0', 42);
+INSERT INTO `tbl_employee` VALUES (104, '康华', '2217021563@qq.com', b'0', 41);
+INSERT INTO `tbl_employee` VALUES (105, '李小龙', '2217021563@qq.com', b'0', 43);
+INSERT INTO `tbl_employee` VALUES (106, '唐太宗', '2217021563@qq.com', b'0', 42);
+INSERT INTO `tbl_employee` VALUES (107, '武则天', '1277771312@qq.com', b'1', 41);
 
 -- ----------------------------
 -- Table structure for user_info
@@ -61,26 +94,20 @@ INSERT INTO `tbl_employee` VALUES (1, '李桂华', '21213@qq.com', b'1', 1);
 DROP TABLE IF EXISTS `user_info`;
 CREATE TABLE `user_info`  (
   `user_id` int(0) NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `user_password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `active_code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `user_status` char(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT 'N',
+  `user_name` varchar(55) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `user_password` varchar(155) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `user_telephone` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `user_code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
   PRIMARY KEY (`user_id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user_info
 -- ----------------------------
-INSERT INTO `user_info` VALUES (1, '测试', '123456', NULL, 'N');
-INSERT INTO `user_info` VALUES (2, 'sds', '123456', NULL, NULL);
-INSERT INTO `user_info` VALUES (3, 'xuyi', '123456', NULL, NULL);
-INSERT INTO `user_info` VALUES (4, 'zff', '123456', NULL, NULL);
-INSERT INTO `user_info` VALUES (5, 'hjgj', '3454', NULL, NULL);
-INSERT INTO `user_info` VALUES (6, 'sas', '2131', NULL, NULL);
-INSERT INTO `user_info` VALUES (7, 'fhsjd', '2111', NULL, NULL);
-INSERT INTO `user_info` VALUES (8, 'xxa', '12121', NULL, NULL);
-INSERT INTO `user_info` VALUES (9, '悬壶济世', '1212', NULL, NULL);
-INSERT INTO `user_info` VALUES (10, '村上春树', 'sasa', NULL, NULL);
-INSERT INTO `user_info` VALUES (11, '啥啊啥', 'sasaa', NULL, NULL);
+INSERT INTO `user_info` VALUES (12, '彭于晏', '123456', '18212629751', NULL);
+INSERT INTO `user_info` VALUES (13, '邹芳芳', '7654321', '18212629751', NULL);
+INSERT INTO `user_info` VALUES (14, '刘德华', '1234567', '17366637245', NULL);
+INSERT INTO `user_info` VALUES (15, '周慧敏', '12345678', '18212629751', NULL);
+INSERT INTO `user_info` VALUES (16, '李小龙', '1234567', '18212629751', NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
